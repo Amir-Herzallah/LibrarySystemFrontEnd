bde2154eccbfaefe7591eef0d503007941d79ba9
Ahmed Abukishk <ahmadabukishk50@gmail.com>
2024-01-05 05:02:51 +0300

    Fixed the home slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [
+
+    '.mat-typography h2 {line-height: 0px}'
+  ]
 })
 export class AppComponent {
   title = 'LibrarySystem';
