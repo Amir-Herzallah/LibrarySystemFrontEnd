777982f44e1b5b955e53cfc15354b6fb24ae2b0e
Ahmad Al-Jelani <ajelani62002@gmail.com>
2024-01-08 23:25:36 +0300

    testimonial page, my trips design and account rout

--- src/app/contactus/contact-us-dialog/contact-us-dialog.component.ts
+++ src/app/contactus/contact-us-dialog/contact-us-dialog.component.ts
@@ -9,18 +9,18 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   templateUrl: './contact-us-dialog.component.html',
   styleUrls: ['./contact-us-dialog.component.css']
 })
+
 export class ContactUsDialogComponent {
   editContactus = new FormGroup({
     contactuspagE_ID: new FormControl(""),
-    email: new FormControl("", Validators.required),
-    subject: new FormControl("", Validators.required),
-    message: new FormControl("", Validators.required),
+    email: new FormControl("example@exp.com", Validators.required),
+    subject: new FormControl("Your Subject", Validators.required),
+    message: new FormControl("Your Messege", Validators.required),
     headeR_COMPONENT1: new FormControl("", Validators.required),
     paragraph1: new FormControl("", Validators.required)
   })
 
   constructor(public contactUs:ContactUsService, @Inject(MAT_DIALOG_DATA) public data: {id: string, header: string, paragraph: string, email: string, subject: string, message: string}) {}
-
   Save() {
     console.log("id: " + this.data.id);
     this.editContactus.controls["contactuspagE_ID"].setValue(this.data.id)
